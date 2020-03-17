353dfc19469b0afeab747ac0b57e17a34891cccc
Felipe Massuia <massuia98.felipe@gmail.com>
2020-03-16 23:40:36 -0300

    Criacao de classe de dominio Categoria

--- src/main/java/com/felipe/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/felipe/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.felipe.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.felipe.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,9 +15,16 @@ public class CategoriaResource {
 	
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List listar() {
 		
-		return "REST está funcionando!";
+		Categoria cat1 = new Categoria(1,"Categoria 1");
+		Categoria cat2 = new Categoria(2,"Categoria 2");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		return lista;
 		
 	}
 
