2020145c50a64a1ceaa11eaca7a3cc1c88a64b6a
Felipe Massuia <massuia98.felipe@gmail.com>
2020-05-25 17:11:22 -0300

    EndPoint clientes/id disponivel

---
+++ src/main/java/com/felipe/cursomc/services/ClienteService.java
@@ -0,0 +1,29 @@
+package com.felipe.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipe.cursomc.domain.Categoria;
+import com.felipe.cursomc.domain.Cliente;
+import com.felipe.cursomc.repositories.ClienteRepository;
+import com.felipe.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional <Cliente> cat = repo.findById(id);
+		//return cat.orElse(null);
+		
+		return cat.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		
+	}
+	
+	
+}
