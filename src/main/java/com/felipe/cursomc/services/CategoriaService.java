b865576c8ca6d062535851b1a3455b9cc00a59b5
Felipe Massuia <massuia98.felipe@gmail.com>
2020-06-10 03:00:01 -0300

    Usando GET para buscar todas as categorias com DTO

--- src/main/java/com/felipe/cursomc/services/CategoriaService.java
+++ src/main/java/com/felipe/cursomc/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.felipe.cursomc.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,6 +47,11 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que tenha produtos!");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		
+		return repo.findAll();
 	}
 	
 	
