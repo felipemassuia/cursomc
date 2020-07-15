533e42f127729708ab31de78bef844fcade86aa7
Felipe Massuia <massuia98.felipe@gmail.com>
2020-07-14 22:52:34 -0300

    Restricao de conteudo - cliente so ve seus pedidos

--- src/main/java/com/felipe/cursomc/services/PedidoService.java
+++ src/main/java/com/felipe/cursomc/services/PedidoService.java
@@ -1,20 +1,29 @@
 package com.felipe.cursomc.services;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.felipe.cursomc.domain.Categoria;
+import com.felipe.cursomc.domain.Cliente;
 import com.felipe.cursomc.domain.ItemPedido;
 import com.felipe.cursomc.domain.PagamentoComBoleto;
 import com.felipe.cursomc.domain.Pedido;
+import com.felipe.cursomc.domain.Produto;
 import com.felipe.cursomc.domain.enums.EstadoPagamento;
 import com.felipe.cursomc.repositories.ItemPedidoRepository;
 import com.felipe.cursomc.repositories.PagamentoRepository;
 import com.felipe.cursomc.repositories.PedidoRepository;
+import com.felipe.cursomc.security.UserSS;
+import com.felipe.cursomc.services.exceptions.AuthorizationException;
 import com.felipe.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -78,6 +87,18 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+			
+			UserSS user = UserService.authenticated();
+			if(user == null) {
+				throw new AuthorizationException("Acesso negado");
+			}
+			
+			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+			Cliente cliente = clienteService.find(user.getId());
+			return repo.findByCliente(cliente, pageRequest);
+		}
 	
 	
 }
